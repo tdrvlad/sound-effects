143db4feb262c4f6ae17793e7228f6b4642ce39a
Vlad <tdrvlad@gmail.com>
2023-12-19 23:06:14 +0200

    .

--- player.py
+++ player.py
@@ -128,5 +128,5 @@ def test_sample(sample_id):
 
 if __name__ == '__main__':
     # load_audio_and_effects("battle1_20231219230207")
-    # test_sample('maxim_machine_gun')
-    test_sample('explosion')
+    test_sample('maxim_machine_gun')
+    # test_sample('explosion')
