04e15b54246f89e4af7a7cf2b0dcb24da05e6259
Vlad <tdrvlad@gmail.com>
2023-12-19 17:23:07 +0200

    .

--- player.py
+++ player.py
@@ -1,8 +1,34 @@
 import os
-from paths import RESULTS_DIR, AUDIO_FILE, TIMESTAMPS_FILE
+import time
+from paths import RESULTS_DIR, AUDIO_FILE, TIMESTAMPS_FILE, EFFECT_1_PIN, EFFECT_2_PIN, EFFECT_3_PIN
 from utils import load_yaml
 from pydub.playback import play
+import threading
 from pydub import AudioSegment
+from rpi import RpiPin
+
+
+pin_mapping = {
+    'ding': EFFECT_1_PIN,
+    'doorbell': EFFECT_2_PIN
+}
+
+
+def control_leds(timestamps, audio_length, pin_mapping):
+    start_time = time.time()
+    while time.time() - start_time < audio_length:
+        current_time = time.time() - start_time
+        for sound, times in timestamps.items():
+            if sound not in pin_mapping:
+                raise ValueError(f"Sound {sound} has no pin associated.")
+            pin_id = pin_mapping[sound]
+
+            pin = RpiPin(pin_id)
+            for start, stop in zip(times['start'], times['stop']):
+                if start <= current_time < stop:
+                    pin.turn_on()
+                elif current_time >= stop:
+                    pin.turn_off()
 
 
 def load_audio_and_effects(sample_id):
@@ -17,7 +43,26 @@ def load_audio_and_effects(sample_id):
         raise ValueError(f"Timestamps {timestamps_file} not found.")
 
     audio = AudioSegment.from_mp3(audio_file)
-    play(audio)
+    timestamps = load_yaml(timestamps_file)
+
+    sounds = list(timestamps.keys())
+    print(f'Loaded timestamps for sounds: {", ".join(sounds)}')
+    if len(sounds) > 3:
+        raise ValueError("System configured for only 3 sound effects")
+
+    pin_mapping = {sound: pin for sound, pin in zip(sounds, [EFFECT_1_PIN, EFFECT_2_PIN, EFFECT_3_PIN])}
+
+    audio_length = len(audio) / 1000.0  # Audio length in seconds
+
+    # Create and start threads
+    audio_thread = threading.Thread(target=play, args=(audio,))
+    led_thread = threading.Thread(target=control_leds, args=(timestamps, audio_length, pin_mapping))
+
+    audio_thread.start()
+    led_thread.start()
+
+    audio_thread.join()
+    led_thread.join()
 
 
 if __name__ == '__main__':
