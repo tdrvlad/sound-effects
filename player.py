6f800b0fe780f55486107d19994cdd025501de4d
Vlad <tdrvlad@gmail.com>
2023-12-20 07:56:52 +0200

    .

--- player.py
+++ player.py
@@ -136,4 +136,4 @@ def test_sample(sample_id):
 if __name__ == '__main__':
     main("battle1_20231219231714")
     # test_sample('maxim_machine_gun')
-    # test_sample('explosion')
+    test_sample('explosion')
