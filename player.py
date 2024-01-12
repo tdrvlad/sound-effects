60161ab9a400a3b83108f144eab97cc03fc3919b
Vlad <tdrvlad@gmail.com>
2024-01-12 13:53:03 +0200

    .

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ import RPi.GPIO as GPIO
 
 
 class AudioPlayer:
-    def __init__(self, audio, delay=-3, volume_change=15):
+    def __init__(self, audio, delay=-3, volume_change=20):
         self.audio = audio
 
         if volume_change:
