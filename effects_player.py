bad3556addf2e64e1293f78e9f7a90a619763aef
Vlad <tdrvlad@gmail.com>
2024-01-23 16:26:25 +0200

    .

--- effects_player.py
+++ effects_player.py
@@ -19,9 +19,11 @@ def create_sound_callable_dict(sound_timestamps_dict, pin: RpiPin):
 def create_sounds_callable_dict(sounds_timestamps_dict, pins):
     callable_dict = {}
     for sound, sound_timestamps_dict in sounds_timestamps_dict.items():
-        # if sound not in pins:
-        #     raise ValueError(f"No GPIO pin associated to sound {sound}")
-        pin = pins[sound]
+        if sound not in pins:
+            print(f"No GPIO pin associated to sound {sound}")
+            pin = None
+        else:
+            pin = pins[sound]
         callable_dict.update(create_sound_callable_dict(sound_timestamps_dict, pin))
     return dict(sorted(callable_dict.items()))
 
